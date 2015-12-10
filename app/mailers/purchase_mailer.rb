f94c6975e8a9e18bfdf841272539d613ef04bde5
Joe Minock <joe.minock@gmail.com>
2015-12-09 23:35:31 -0700

    fixed stupid bug on mailers > purchase_mailer

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -1,7 +1,7 @@
 class PurchaseMailer < ActionMailer::Base
 layout 'purchase_mailer'
 
- default from: "Joe Payments <joe.minock+payments@gmail.com"
+ default from: "Joe Payments <joe.minock+payments@gmail.com>"
 
  def purchase_receipt purchase
 	@purchase = purchase
