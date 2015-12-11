038a4ebb282434bfadd70add56ee6c7701326a01
Joe Minock <joe.minock@gmail.com>
2015-12-10 20:52:10 -0700

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
-growth_hacking = Product.create(title: "Growth Hacking", 
- subtitle: "Crash Course", author: "Mattan Griffel", 
- length: "30 minutes", author_image_name: "teacher-image.jpg", 
- price: "4.99", sku: "GROHACK1", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
- details: "You'll get one video", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly", 
+ subtitle: "Ongoing Course", author: "Mattan Griffel", 
+ length: "6 months", author_image_name: "teacher-image.jpg", 
+ price: "4.99", sku: "GROHACK2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+ details: "We'll charge you monthly for unlimited access to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
 
- <p>In this talk, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+ <p>In this series, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
  <p><strong>What You'll Learn</strong></p>
  <ul class="no-indent">
  <li>What is a growth hacker?</li>
@@ -20,16 +20,35 @@ growth_hacking = Product.create(title: "Growth Hacking",
  </p>
  <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
 
-growth_hacking_monthly = Product.create(title: "Growth Hacking Monthly", 
+copy_writing_monthly = Product.create(title: "Copy Writing Monthly", 
  subtitle: "Ongoing Course", author: "Mattan Griffel", 
  length: "6 months", author_image_name: "teacher-image.jpg", 
- price: "4.99", sku: "GROHACK2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+ price: "4.99", sku: "COPYHACK2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
  details: "We'll charge you monthly for unlimited access to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
 
- <p>In this series, Mattan Griffel introduces you to the concept of Growth Hacking and shares his favorite tips for getting started as a growth hacker.</p>
+ <p>In this series, Mattan Griffel introduces you to the concept of Copy Writing and shares his favorite tips for getting started as a growth hacker.</p>
  <p><strong>What You'll Learn</strong></p>
  <ul class="no-indent">
- <li>What is a growth hacker?</li>
+ <li>What is a Copy Writer?</li>
+ <li>The 5 stages of the user lifecycle</li>
+ <li>How to apply the Lean Marketing Framework</li>
+ <li>Resources and tools you'll need to know</li>
+ </ul>}, 
+ author_description: %{ <p>Hey, I'm Mattan Griffel, co-founder of the Y Combinator backed <a href="http://www.onemonth.com" target="_blank">One Month</a> where I teach the best selling One Month Rails and One Month Growth Hacking.</p>
+ <p>Why take my class? In the past I've advised companies like Pepsico, Bloomberg, GM, NYSE, and JPMorgan on growth hacking best practices. I've also spoken at New York University, Cooper Union and First Round Capital. My goal is to help you and other entrepreneurs rapidly grow your business. 
+ </p>
+ <p>Follow me on Twitter <a href="https://twitter.com/mattangriffel" target="_blank">@mattangriffel</a></p>})
+
+ analytics_monthly = Product.create(title: "Analytics Monthly", 
+ subtitle: "Ongoing Course", author: "Mattan Griffel", 
+ length: "6 months", author_image_name: "teacher-image.jpg", 
+ price: "4.99", sku: "ANALYTICSHACK2", download_url: "https://s3.amazonaws.com/one-month-stripe-payments/Peter+Bell+Resume.pdf", 
+ details: "We'll charge you monthly for unlimited access to our mentoring team", description: %{<p>A growth hacker is a rare combination: someone with the right marketing and technical skills who can come up with clever marketing hacks and also track their results.</p>
+
+ <p>In this series, Mattan Griffel introduces you to the concept of Copy Writing and shares his favorite tips for getting started as a growth hacker.</p>
+ <p><strong>What You'll Learn</strong></p>
+ <ul class="no-indent">
+ <li>What is a Copy Writer?</li>
  <li>The 5 stages of the user lifecycle</li>
  <li>How to apply the Lean Marketing Framework</li>
  <li>Resources and tools you'll need to know</li>
