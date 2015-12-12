28903ef0b3758666710ec275c4a04651c730b8fd
Joe Minock <joe.minock@gmail.com>
2015-12-11 22:19:50 -0700

    extra credit, scoped products to active admin. Working on Create Method in Product Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,8 +1,12 @@
 class	ProductsController < ApplicationController
+	def create
+		@product = Product.new(product_params)
+	end
 	def index
 		@products = Product.all	
 	end
 	def show
 		@product = Product.find_by(params[:id])
 	end
+	
 end
